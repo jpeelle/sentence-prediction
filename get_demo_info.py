9bbd58cf7d3a6d969866eaf2c91a852539475d6b
Ryland Miller <rylandm@uoregon.edu>
2019-04-21 16:04:53 -0500

    Fixes bug with creating demo_file.csv using multiple input data files

--- get_demo_info.py
+++ get_demo_info.py
@@ -2,13 +2,13 @@ import sys
 
 def get_demo_info(csvfiles):
     with open('demo_file.csv', 'w') as dfile:
+        demo_info = {}
         for csvfile in csvfiles:
             with open(csvfile, 'r') as cfile:
                 header = cfile.readline()
                 header_list = header.split('","')
                 demo_categories = [i.replace('Answer.', '') for i in header_list if 'Answer' in i and 'sentence' not in i]
                 demo_indices = [header_list.index(i) for i in header_list if 'Answer' in i and 'sentence' not in i]
-                demo_info = {}
                 num_questions = len([i for i in header_list if 'sentence' in i]) // 2
                 for line in cfile:
                     line_list = line.split('","')
@@ -26,10 +26,10 @@ def get_demo_info(csvfiles):
                     for i in demo_categories:
                         dfile.write(',{}'.format(i))
                     dfile.write('\n')
-                for k,v in demo_info.items():
-                    dfile.write('{}'.format(k))
-                    for i in v:
-                        dfile.write(',{}'.format(i))
-                    dfile.write('\n')
+        for k,v in demo_info.items():
+            dfile.write('{}'.format(k))
+            for i in v:
+                dfile.write(',{}'.format(i))
+            dfile.write('\n')
 
 get_demo_info(['example/anonymized_fill-in-1x35.csv','example/anonymized_fill-in-1x50.csv','example/anonymized_fill-in-61x50.csv'])

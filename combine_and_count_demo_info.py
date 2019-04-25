060e4c91e3707cdd444ec268e8d26078110bbd80
Ryland Miller <rylandm@uoregon.edu>
2019-04-24 19:31:20 -0500

    Makes a column in consolidated_demographics.csv for if and where answer mismatches occurred for each participant

--- combine_and_count_demo_info.py
+++ combine_and_count_demo_info.py
@@ -11,17 +11,19 @@
 #             wf.write('{},{}\n'.format(line_list[0],num_hits))
 
 # Combine
-with open('example/consolidated_demographics.csv', 'w') as wf:
+with open('consolidated_demographics.csv', 'w') as wf:
     with open('example/demographics.csv', 'r') as rf:
         header = rf.readline()
-        wf.write(header)
         header_list = header.split(',')
+        header_list[0] = 'AnswerMismatch'
+        header_list = ['SubjectID'] + header_list
+        wf.write(','.join(header_list))
 
         for line in rf:
             line_list = line.split(',')
             subject_id = line_list[0]
             line_list = line_list[1:]
-            mismatched_answer = False
+            mismatched_answer = 'N/A'
 
             num_questions = 10
             for i in range(0, len(line_list), num_questions):
@@ -37,13 +39,20 @@ with open('example/consolidated_demographics.csv', 'w') as wf:
                                 if i != 0:
                                     count += 1
                             else:
-                                mismatched_answer = True
+                                if type(mismatched_answer) == set:
+                                    mismatched_answer.add(header_list[j+2].strip())
+                                else:
+                                    mismatched_answer = set([header_list[j+2].strip()])
                                 count += 0
 
                 if count == len(question_set):
                     for k in range(i,i+num_questions):
                         line_list[k] = None
-            if mismatched_answer:
-                line_list = ['answer mismatch'] + line_list
 
+            if type(mismatched_answer) == set:
+                line_list = [';'.join(mismatched_answer)] + line_list
+            else:
+                line_list = [mismatched_answer] + line_list
+            #if mismatched_answer:
+            #    line_list = ['answer mismatch'] + line_list
             wf.write(subject_id + ',' + ','.join([x for x in line_list if x is not None ]))
